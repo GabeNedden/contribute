a4a18aeced9993831f4b414f388ef7c105e591c4
Dayanand M.G <dayanandadaya8055@gmail.com>
2021-10-18 10:50:33 +0530

    My commit to contributers.js

--- contributors.js
+++ contributors.js
@@ -77,6 +77,10 @@ const contributors = [
     {
         name: "Jasleen Sondhi",
         github:"https://github.com/jasleen101010"
+    },
+    {
+        name: "Dayanand M.G",
+        github:"https://github.com/dayanand-g"
     }
 
 ]
