85a0f527e33e5153153cdc373a8832ed1cfcd148
Edilson Matola <edilsonmatola13@gmail.com>
2022-02-27 13:27:32 +0200

    Add Edilson Matola to Contributors list

--- contributors.js
+++ contributors.js
@@ -1,179 +1,182 @@
 // Add your name and bio to the list of objects!
 
 const contributors = [
-    {
-        name: "Gabriel Zur Nedden",
-        github: "https://github.com/GabeNedden"
-    },
-    {
-        name: "Haley Harrington",
-        github: "https://github.com/haleyharrington"
-    },
-    {
-        name: "Pranshu Jha",
-        github: "https://github.com/pranshuj73"
-    },
-    {
-        name: "Mohamed Danis",
-        github: "https://github.com/MohamedDanis"
-    },
-    {
-        name: "Jash Lodhavia",
-        github: "https://github.com/jashlodhavia"
-    },
-    {
-        name: "Caleb Felix",
-        github: "https://github.com/calebfelix"
-    },
-    {
-        name: "Pururaj Singh Rajawat",
-        github: "https://github.com/pururaj987"
-    },
-    {
-        name: "Sooraj Gk",
-        github: "https://github.com/sooraj-gk"
-    },
-    {
-        name: "Aman Agrawal",
-        github: "https://github.com/aman0408"
-    },
-    {
-        name: "Pravash Purkayastha",
-        github: "https://github.com/pravash123"
-
-    },
-    {
-        name: "Shahbaz Alam",
-        github: "https://github.com/Alam9835"
-    },
-    {
-        name: "Aaron Philip",
-        github: "https://github.com/Aaronphilip2003"
-    },
-    {
-        name: "Juli Cheng",
-        github: "https://github.com/julicheng"
-    },
-    {
-        name: "Ayaka Nishimura",
-        github: "https://github.com/ayaka28"
-    },
-    {
-        name: "Suresh MuthuSelvam",
-        github: "https://github.com/suresh37"
-    },
-    {
-        name: "Zezou",
-        github: "https://github.com/Zezou"
-    },
-    {
-        name: "Shahbaz Alam",
-        github: "https://github.com/Alam9835"
-    },
-    {
-        name: "Matheus Mazoni",
-        github: "https://github.com/MMazoni"
-    },
-    {
-        name: "Jasleen Sondhi",
-        github: "https://github.com/jasleen101010"
-    },
-    {
-        name: "Tushar Sharma",
-        github: "https://github.com/tusharsharmain"
-    },
-    {
-        name: "Dayanand M.G",
-        github: "https://github.com/dayanand-g"
-    },
-    {
-        name: "Luis Izquierdo",
-        github: "https://github.com/leip1493"
-    },
-    {
-        name: "Deep Shikha",
-        github: "https://github.com/DeepShikhaIN"
-    },
-    {
-        name: "Shivansh Yadav",
-        github: "https://github.com/Shivansh-yadav13"
-    },
-    {
-        name: "Umapreethi Santhanakrishnan",
-        github: "https://github.com/umapreethi-dev/"
-    },
-    {
-        name: "Shrayansh Tyagi",
-        github: "https://github.com/Shray-glitch"
-    },
-    {
-        name: "Sahil Sheth",
-        github: "https://github.com/shlsheth263"
-    },
-    {
-        name: "Zesty Coder",
-        github: "https://github.com/zestycoder"
-    },
-    {
-        name: "Kike Sanchez",
-        github: "https://github.com/KikeSan"
-    },
-    {
-        name: "Anubhav Rajput",
-        github: "https://github.com/anubhav180400"
-    },
-    {
-        name: "Davy Sharma",
-        github: "https://github.com/davysharma4/"
-    },
-    {
-        name: "Shreshth Gilhotra",
-        github: "https://github.com/shreshth-12"
-    },
-    {
-        name: "Shreya Singh",
-        github: "https://github.com/real-phoenix"
-    },
-    {
-        name: "Diamond Rivero",
-        github: "https://github.com/diamant3"
-    },
-    {
-        name: "Advait Khawase",
-        github: "https://github.com/advaitkhawase15"
-    },
-    {
-        name: "Shrey Nahar A",
-        github: "https://github.com/Shrey-Nahar"
-    },
-    {
-        name: "Tchassem Johan",
-        github: "https://github.com/johan237"
-    },
-    {
-        name: "Vignesh C K",
-        github: "https://github.com/ckvignesh"
-    },
-    {
-        name: "Deepanshu Sharma",
-        github: "https://github.com/deepanshu-sharmaa"
-    },
-     {
-        name: "AkshayHere",
-        github: "https://github.com/AkshayHere"
-    },
-    {
-        name: "Ayush0431",
-        github: "https://github.com/Ayush0431"
-    },
-    {
-        name: "Ranuga Disansa",
-        github: "https://github.com/Programmer-RD-AI"
-    },
-    {
-        name: "sohamkolambe",
-        github: "https://github.com/sohamkolambe"
-    }
-]
+  {
+    name: "Gabriel Zur Nedden",
+    github: "https://github.com/GabeNedden",
+  },
+  {
+    name: "Haley Harrington",
+    github: "https://github.com/haleyharrington",
+  },
+  {
+    name: "Pranshu Jha",
+    github: "https://github.com/pranshuj73",
+  },
+  {
+    name: "Mohamed Danis",
+    github: "https://github.com/MohamedDanis",
+  },
+  {
+    name: "Jash Lodhavia",
+    github: "https://github.com/jashlodhavia",
+  },
+  {
+    name: "Caleb Felix",
+    github: "https://github.com/calebfelix",
+  },
+  {
+    name: "Pururaj Singh Rajawat",
+    github: "https://github.com/pururaj987",
+  },
+  {
+    name: "Sooraj Gk",
+    github: "https://github.com/sooraj-gk",
+  },
+  {
+    name: "Aman Agrawal",
+    github: "https://github.com/aman0408",
+  },
+  {
+    name: "Pravash Purkayastha",
+    github: "https://github.com/pravash123",
+  },
+  {
+    name: "Shahbaz Alam",
+    github: "https://github.com/Alam9835",
+  },
+  {
+    name: "Aaron Philip",
+    github: "https://github.com/Aaronphilip2003",
+  },
+  {
+    name: "Juli Cheng",
+    github: "https://github.com/julicheng",
+  },
+  {
+    name: "Ayaka Nishimura",
+    github: "https://github.com/ayaka28",
+  },
+  {
+    name: "Suresh MuthuSelvam",
+    github: "https://github.com/suresh37",
+  },
+  {
+    name: "Zezou",
+    github: "https://github.com/Zezou",
+  },
+  {
+    name: "Shahbaz Alam",
+    github: "https://github.com/Alam9835",
+  },
+  {
+    name: "Matheus Mazoni",
+    github: "https://github.com/MMazoni",
+  },
+  {
+    name: "Jasleen Sondhi",
+    github: "https://github.com/jasleen101010",
+  },
+  {
+    name: "Tushar Sharma",
+    github: "https://github.com/tusharsharmain",
+  },
+  {
+    name: "Dayanand M.G",
+    github: "https://github.com/dayanand-g",
+  },
+  {
+    name: "Luis Izquierdo",
+    github: "https://github.com/leip1493",
+  },
+  {
+    name: "Deep Shikha",
+    github: "https://github.com/DeepShikhaIN",
+  },
+  {
+    name: "Shivansh Yadav",
+    github: "https://github.com/Shivansh-yadav13",
+  },
+  {
+    name: "Umapreethi Santhanakrishnan",
+    github: "https://github.com/umapreethi-dev/",
+  },
+  {
+    name: "Shrayansh Tyagi",
+    github: "https://github.com/Shray-glitch",
+  },
+  {
+    name: "Sahil Sheth",
+    github: "https://github.com/shlsheth263",
+  },
+  {
+    name: "Zesty Coder",
+    github: "https://github.com/zestycoder",
+  },
+  {
+    name: "Kike Sanchez",
+    github: "https://github.com/KikeSan",
+  },
+  {
+    name: "Anubhav Rajput",
+    github: "https://github.com/anubhav180400",
+  },
+  {
+    name: "Davy Sharma",
+    github: "https://github.com/davysharma4/",
+  },
+  {
+    name: "Shreshth Gilhotra",
+    github: "https://github.com/shreshth-12",
+  },
+  {
+    name: "Shreya Singh",
+    github: "https://github.com/real-phoenix",
+  },
+  {
+    name: "Diamond Rivero",
+    github: "https://github.com/diamant3",
+  },
+  {
+    name: "Advait Khawase",
+    github: "https://github.com/advaitkhawase15",
+  },
+  {
+    name: "Shrey Nahar A",
+    github: "https://github.com/Shrey-Nahar",
+  },
+  {
+    name: "Tchassem Johan",
+    github: "https://github.com/johan237",
+  },
+  {
+    name: "Vignesh C K",
+    github: "https://github.com/ckvignesh",
+  },
+  {
+    name: "Deepanshu Sharma",
+    github: "https://github.com/deepanshu-sharmaa",
+  },
+  {
+    name: "AkshayHere",
+    github: "https://github.com/AkshayHere",
+  },
+  {
+    name: "Ayush0431",
+    github: "https://github.com/Ayush0431",
+  },
+  {
+    name: "Ranuga Disansa",
+    github: "https://github.com/Programmer-RD-AI",
+  },
+  {
+    name: "sohamkolambe",
+    github: "https://github.com/sohamkolambe",
+  },
+  {
+    name: "Edilson Matola",
+    github: "https://github.com/edilsonmatola",
+  },
+];
 
 console.log(contributors)
