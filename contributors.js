f91f7bd07ce4660f5f2f3680df367756f41138fe
unknown <terrancebryant165@gmail.com>
2023-09-15 15:35:59 +0300

    Contribution updated

--- contributors.js
+++ contributors.js
@@ -180,6 +180,10 @@ const contributors = [
   {
     name: "Harshbardhan Singh",
     github: "https://github.com/harsh1x4"
+  },
+  {
+    name: "Terrance Bryan",
+    github: "https://github.com/TerranceBryan",
   }
 ];
 
---
+++ menu.js
@@ -0,0 +1,14 @@
+
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick =()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll =()=>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
